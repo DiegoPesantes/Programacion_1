339152a4e9c95b65fa2e0213d3cb7d91d3afa009
Diego Pesantes <diego.pesantes@udla.edu.ec>
2023-10-12 16:44:50 -0500

    Creacion de la respuesta

--- Suma_Gauss.c
+++ Suma_Gauss.c
@@ -5,8 +5,10 @@ int numero;
 int suma=0
 printf ("Ingresa un numero");
 scanf ("%d" &numero);
-for (int i=1; i<= numero; i++);
+for (int i=1; i<= numero; i++){
 suma+= i;
-
+}
+printf ("La suma de tu numero %d es igual a:", numero, suma);
+return 0;
 }
 
